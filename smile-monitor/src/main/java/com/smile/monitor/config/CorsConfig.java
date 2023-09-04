c99e587b8f85451f86dbe0372e8bc6500dba1ba3
xiezhi <xiezhi@dstcar.com>
2023-09-04 17:28:00 +0800

    调整

---
+++ smile-monitor/src/main/java/com/smile/monitor/config/CorsConfig.java
@@ -0,0 +1,73 @@
+package com.smile.monitor.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.apache.catalina.valves.RemoteIpValve;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author :xiezhi
+ * @date : 2023/9/4
+ */
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
+                .allowedHeaders("origin", "content-type", "accept", "x-requested-with")
+                .maxAge(3600);
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        List<String> list = new ArrayList<>();
+        list.add("*");
+        corsConfiguration.setAllowedOrigins(list);
+
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory servletContainer(){
+        TomcatServletWebServerFactory  factory = new TomcatServletWebServerFactory ();
+        factory.setUriEncoding(StandardCharsets.UTF_8);
+        RemoteIpValve value = new RemoteIpValve();
+        value.setRemoteIpHeader("X-Forwarded-For");
+        value.setProtocolHeader("X-Forwarded-Proto");
+        value.setProtocolHeaderHttpsValue("https");
+        factory.addEngineValves(value);
+        return factory;
+    }
+
+
+}
