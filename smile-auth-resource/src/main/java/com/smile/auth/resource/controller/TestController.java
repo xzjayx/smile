16b8026434154e9e658761e226b22ccd2b2464f7
xiezhi <xiezhi@dstcar.com>
2023-12-27 09:36:37 +0800

    引入 demo模块，作为测试编写。不涉及主要逻辑

--- smile-auth-resource/src/main/java/com/smile/auth/resource/controller/TestController.java
+++ smile-auth-resource/src/main/java/com/smile/auth/resource/controller/TestController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author xiezhi
+ */
 @RestController
 @RequestMapping("/test")
 public class TestController {
